dedbc20cd553ef6e10826b4bfa41d2604754efa9
kir <kpoltoradnev@gmail.com>
2020-07-31 00:39:27 +0500

    #2 creation UI

--- app/src/main/java/com/kir/simplecameraapp/MainActivity.java
+++ app/src/main/java/com/kir/simplecameraapp/MainActivity.java
@@ -12,6 +12,8 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.view.TextureView;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -61,7 +63,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
         checkPermission();
 
         mTextureView = findViewById(R.id.textureView);
@@ -72,13 +78,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if(CameraHandler.mIsRecording) {
                     CameraHandler.mIsRecording = false;
-                    mRecordImageButton.setImageResource(R.drawable.btn_camera_ready);
+                    mRecordImageButton.setImageResource(R.drawable.camera_btn_ready);
                     CameraHandler.startPreview();
                     FileManagerHandler.mMediaRecorder.stop();
                     FileManagerHandler.mMediaRecorder.reset();
                 } else {
                     CameraHandler.mIsRecording = true;
-                    mRecordImageButton.setImageResource(R.drawable.btn_camera_busy);
+                    mRecordImageButton.setImageResource(R.drawable.camera_btn_busy);
                     try {
                         FileManagerHandler.createVideoFileName();
                     } catch (IOException e) {
@@ -112,6 +118,26 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        View decorView = getWindow().getDecorView();
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_IMMERSIVE
+                            // Set the content to appear under the system bars so that the
+                            // content doesn't resize when the system bars hide and show.
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            // Hide the nav bar and status bar
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        }
+    }
+
+    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if(requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
