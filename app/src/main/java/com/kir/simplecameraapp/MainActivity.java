7194e62ce812c805891af7ffaf96c2cc89b303a6
kir <kpoltoradnev@gmail.com>
2020-07-31 13:19:07 +0500

    #3.0 add AdView and made final UI

--- app/src/main/java/com/kir/simplecameraapp/MainActivity.java
+++ app/src/main/java/com/kir/simplecameraapp/MainActivity.java
@@ -21,6 +21,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.kir.simplecameraapp.utillity.CameraHandler;
 import com.kir.simplecameraapp.utillity.FileManagerHandler;
 
@@ -60,6 +65,8 @@ public class MainActivity extends AppCompatActivity {
     private HandlerThread mBackgroundHandlerThread;
     private Handler mBackgroundHandler;
 
+    private AdView mAdView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,8 +74,16 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-
         checkPermission();
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         mTextureView = findViewById(R.id.textureView);
         CameraHandler.mTextureView = mTextureView;
